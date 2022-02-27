87dd45ef3b474d575bf7703f66135c3f41245825
DevLeuzera <le.tex@hotmail.com>
2022-02-27 19:15:11 -0300

    test: add integration test of add product route

---
+++ tests/main/routes/product-routes.test.ts
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { PgProduct } from '@/infra/database/entities'
+import { PostgresHelper } from '@/infra/database/helpers/postgres-helper'
+import { buildApp } from '@/main/config'
+import { testEnv } from '@/tests/utils'
+
+import { datatype, random } from 'faker'
+import path from 'path'
+import request from 'supertest'
+import { Repository } from 'typeorm'
+
+let productRepository: Repository<any>
+let productsCategoriesCategoriesRepository: Repository<any>
+
+describe('Product Routes', () => {
+  beforeAll(async () => {
+    await PostgresHelper.connect(testEnv.postgresTestHost, testEnv.postgresTestPort, testEnv.postgresTestUsername, testEnv.postgresTestPassword, testEnv.postgresTestDatabase, [path.join(__dirname, '../../../src/infra/database/entities/*{.js,.ts}')], true)
+  })
+
+  afterAll(async () => {
+    await PostgresHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    productsCategoriesCategoriesRepository = await PostgresHelper.getRepository('products_categories_categories')
+    await productsCategoriesCategoriesRepository.delete({})
+    productRepository = await PostgresHelper.getRepository(PgProduct)
+    await productRepository.delete({})
+  })
+
+  describe('POST /products/add-product', () => {
+    test('Should return 200 on success', async () => {
+      const app = await buildApp()
+      await request(app)
+        .post('/products/add-product')
+        .send({
+          name: random.words(),
+          sku: random.words(),
+          price: datatype.number(),
+          description: random.words(),
+          quantity: datatype.number()
+        })
+        .expect(200)
+    })
+  })
+})
