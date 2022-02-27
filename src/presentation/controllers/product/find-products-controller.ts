7a88dd60c6dc0bcd3fc5b0a905d59c7ac6b99142
DevLeuzera <le.tex@hotmail.com>
2022-02-27 20:51:44 -0300

    feat: add FindProductsController

---
+++ src/presentation/controllers/product/find-products-controller.ts
@@ -0,0 +1,31 @@
+import { RequestQuery } from '@/domain/models'
+import { FindProducts } from '@/domain/usecases'
+import { HttpError } from '@/presentation/errors'
+import { ok } from '@/presentation/helpers'
+import { Http } from '@/presentation/protocols'
+import { Validation } from '@/validation/protocols'
+
+export class FindProductsController implements Http.Controller {
+  constructor (
+    private readonly validation: Validation<FindProductsController.Request>,
+    private readonly findProducts: FindProducts
+  ) {}
+
+  async handle (request: FindProductsController.Request): Promise<Http.Response<FindProductsController.Response>> {
+    try {
+      const badParams = await this.validation.validate(request)
+      if (badParams) return new HttpError.BadRequest(badParams)
+
+      const result = await this.findProducts.find({ query: request })
+
+      return ok(result)
+    } catch (error) {
+      return new HttpError.Server(String(error.stack))
+    }
+  }
+}
+
+export namespace FindProductsController {
+  export type Request = Partial<RequestQuery.Search & RequestQuery.Limit & RequestQuery.Page>
+  export type Response = FindProducts.Result
+}
--- src/presentation/controllers/product/index.ts
+++ src/presentation/controllers/product/index.ts
@@ -1,1 +1,2 @@
 export * from './add-product-controller'
+export * from './find-products-controller'
