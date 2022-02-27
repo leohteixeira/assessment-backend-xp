b334c5d89dd8dfa6015f9c1cc78776f4cf3dd9d0
DevLeuzera <le.tex@hotmail.com>
2022-02-27 20:47:17 -0300

    feat: add findProducts() method

--- src/infra/database/repositories/product-repository.ts
+++ src/infra/database/repositories/product-repository.ts
@@ -1,10 +1,11 @@
 import { DatabaseError } from '@/data/errors'
-import { AddProductRepository } from '@/data/protocols'
+import { AddProductRepository, FindProductsRepository } from '@/data/protocols'
 import { PgProduct, PgCategory } from '@/infra/database/entities'
+import { PostgresHelper } from '@/infra/database/helpers'
 
 import { getManager } from 'typeorm'
 
-export class PgProductRepository implements AddProductRepository {
+export class PgProductRepository implements AddProductRepository, FindProductsRepository {
   async add (params: AddProductRepository.Params): Promise<AddProductRepository.Result> {
     try {
       let product
@@ -35,6 +36,28 @@ export class PgProductRepository implements AddProductRepository {
       } else {
         throw new DatabaseError.InsertFail(String(error.stack))
       }
+    }
+  }
+
+  async findProducts (params: FindProductsRepository.Params): Promise<FindProductsRepository.Result> {
+    const productRepository = await PostgresHelper.getRepository(PgProduct)
+    const page = (params.query.page && params.query.page > 0) ? params.query.page : 1
+    const pageSize = params.query.limit ? params.query.limit : 20
+    const skipSize = pageSize * (page - 1)
+
+    let queryBuilder = productRepository.createQueryBuilder('product').leftJoinAndSelect('product.categories', 'categories')
+
+    if (params.query.search && (params.query.searchValue || params.query.searchValue === 0)) {
+      queryBuilder = queryBuilder.where(`product.${params.query.search} like :searchValue`, { searchValue: `%${params.query.searchValue}%` })
+    }
+
+    const result = await queryBuilder.skip(skipSize).take(pageSize).getManyAndCount()
+
+    return {
+      elements: result[0],
+      totalElements: result[1],
+      totalPages: result[1] === 0 ? 1 : Math.ceil(result[1] / pageSize),
+      currentPage: page
     }
   }
 }
