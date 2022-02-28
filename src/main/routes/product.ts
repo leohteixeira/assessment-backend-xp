a98123d9f597643034c5fd13c986be04739dee30
DevLeuzera <le.tex@hotmail.com>
2022-02-27 22:01:56 -0300

    feat: add find product route

--- src/main/routes/product.ts
+++ src/main/routes/product.ts
@@ -1,9 +1,10 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeAddProductController, makeFindProductsController } from '@/main/factories/controllers'
+import { makeAddProductController, makeFindProductsController, makeFindProductController } from '@/main/factories/controllers'
 
 import { Router } from 'express'
 
 export const applyProductRoutes = (router: Router): void => {
   router.get('/products', adaptRoute(makeFindProductsController()))
+  router.get('/products/:productId', adaptRoute(makeFindProductController()))
   router.post('/products/add-product', adaptRoute(makeAddProductController()))
 }
