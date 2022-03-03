1e606ae4ec1a5735dbcf0e8ee78dc42b3844a876
DevLeuzera <le.tex@hotmail.com>
2022-03-03 17:09:54 -0300

    feat: add DbRemoveProduct

---
+++ src/data/usecases/product/db-remove-product.ts
@@ -0,0 +1,12 @@
+import { RemoveProductRepository } from '@/data/protocols'
+import { RemoveProduct } from '@/domain/usecases'
+
+export class DbRemoveProduct implements RemoveProduct {
+  constructor (
+    private readonly removeProductRepository: RemoveProductRepository
+  ) {}
+
+  async remove (params: RemoveProduct.Params): Promise<RemoveProduct.Result> {
+    await this.removeProductRepository.removeProduct(params)
+  }
+}
