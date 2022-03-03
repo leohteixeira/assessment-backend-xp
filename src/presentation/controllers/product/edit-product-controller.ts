d85202e07259266ff1d79f6c307c13e45f3be4a5
DevLeuzera <le.tex@hotmail.com>
2022-03-03 15:28:12 -0300

    feat: add EditProductController

---
+++ src/presentation/controllers/product/edit-product-controller.ts
@@ -0,0 +1,32 @@
+import { EditProduct } from '@/domain/usecases'
+import { HttpError } from '@/presentation/errors'
+import { ok } from '@/presentation/helpers'
+import { Http } from '@/presentation/protocols'
+import { Validation } from '@/validation/protocols'
+
+export class EditProductController implements Http.Controller {
+  constructor (
+    private readonly validation: Validation<EditProductController.Request>,
+    private readonly editProduct: EditProduct
+  ) {}
+
+  async handle (request: EditProductController.Request): Promise<Http.Response<EditProductController.Response>> {
+    try {
+      const badParams = await this.validation.validate(request)
+      if (badParams) return new HttpError.BadRequest(badParams)
+
+      const result = await this.editProduct.edit({
+        ...request
+      })
+
+      return ok(result)
+    } catch (error) {
+      return new HttpError.Server(String(error.stack))
+    }
+  }
+}
+
+export namespace EditProductController {
+  export type Request = EditProduct.Params
+  export type Response = EditProduct.Result
+}
--- src/presentation/controllers/product/index.ts
+++ src/presentation/controllers/product/index.ts
@@ -1,3 +1,4 @@
 export * from './add-product-controller'
 export * from './find-products-controller'
 export * from './find-product-controller'
+export * from './edit-product-controller'
