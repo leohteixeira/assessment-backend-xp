2e0980ae82bf7c55de04555d8f8601095f5d2777
DevLeuzera <le.tex@hotmail.com>
2022-03-03 23:01:56 -0300

    feat: add findCategories method

--- src/infra/database/repositories/category-repository.ts
+++ src/infra/database/repositories/category-repository.ts
@@ -1,9 +1,9 @@
 import { DatabaseError } from '@/data/errors'
-import { AddCategoryRepository, FindCategoryRepository } from '@/data/protocols'
+import { AddCategoryRepository, FindCategoryRepository, FindCategoriesRepository } from '@/data/protocols'
 import { PgCategory } from '@/infra/database/entities'
 import { PostgresHelper } from '@/infra/database/helpers'
 
-export class PgCategoryRepository implements AddCategoryRepository, FindCategoryRepository {
+export class PgCategoryRepository implements AddCategoryRepository, FindCategoryRepository, FindCategoriesRepository {
   async add (params: AddCategoryRepository.Params): Promise<AddCategoryRepository.Result> {
     try {
       const categoryRepository = await PostgresHelper.getRepository(PgCategory)
@@ -23,5 +23,27 @@ export class PgCategoryRepository implements AddCategoryRepository, FindCategory
 
     if (!category) throw new DatabaseError.NotFound(`"${params.categoryId}" could not be found`)
     return category
+  }
+
+  async findCategories (params: FindCategoriesRepository.Params): Promise<FindCategoriesRepository.Result> {
+    const categoryRepository = await PostgresHelper.getRepository(PgCategory)
+    const page = (params.query.page && params.query.page > 0) ? params.query.page : 1
+    const pageSize = params.query.limit ? params.query.limit : 20
+    const skipSize = pageSize * (page - 1)
+
+    let queryBuilder = categoryRepository.createQueryBuilder('category')
+
+    if (params.query.search && (params.query.searchValue || params.query.searchValue === 0)) {
+      queryBuilder = queryBuilder.where(`category.${params.query.search} like :searchValue`, { searchValue: `%${params.query.searchValue}%` })
+    }
+
+    const result = await queryBuilder.skip(skipSize).take(pageSize).getManyAndCount()
+
+    return {
+      elements: result[0],
+      totalElements: result[1],
+      totalPages: result[1] === 0 ? 1 : Math.ceil(result[1] / pageSize),
+      currentPage: page
+    }
   }
 }
