ce8fb65e4993e69b2e2689b9c42d6c08421aeeda
DevLeuzera <le.tex@hotmail.com>
2022-03-03 22:35:42 -0300

    feat: add FindCategoryController

---
+++ src/presentation/controllers/category/find-category-controller.ts
@@ -0,0 +1,30 @@
+import { FindCategory } from '@/domain/usecases'
+import { HttpError } from '@/presentation/errors'
+import { ok } from '@/presentation/helpers'
+import { Http } from '@/presentation/protocols'
+import { Validation } from '@/validation/protocols'
+
+export class FindCategoryController implements Http.Controller {
+  constructor (
+    private readonly validation: Validation<FindCategoryController.Request>,
+    private readonly findCategory: FindCategory
+  ) {}
+
+  async handle (request: FindCategoryController.Request): Promise<Http.Response<FindCategoryController.Response>> {
+    try {
+      const badParams = await this.validation.validate(request)
+      if (badParams) return new HttpError.BadRequest(badParams)
+
+      const result = await this.findCategory.find({ categoryId: request.categoryId })
+
+      return ok(result)
+    } catch (error) {
+      return new HttpError.Server(String(error.stack))
+    }
+  }
+}
+
+export namespace FindCategoryController {
+  export type Request = FindCategory.Params
+  export type Response = FindCategory.Result
+}
