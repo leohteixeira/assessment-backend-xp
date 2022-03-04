adcabc3e975c040fa441c03ac08f7d642f4134b4
DevLeuzera <le.tex@hotmail.com>
2022-03-04 00:25:23 -0300

    feat: add RemoveCategoryController

---
+++ src/presentation/controllers/category/remove-category-controller.ts
@@ -0,0 +1,30 @@
+import { RemoveCategory } from '@/domain/usecases'
+import { HttpError } from '@/presentation/errors'
+import { noContent } from '@/presentation/helpers'
+import { Http } from '@/presentation/protocols'
+import { Validation } from '@/validation/protocols'
+
+export class RemoveCategoryController implements Http.Controller {
+  constructor (
+    private readonly validation: Validation<RemoveCategoryController.Request>,
+    private readonly removeCategory: RemoveCategory
+  ) {}
+
+  async handle (request: RemoveCategoryController.Request): Promise<Http.Response<RemoveCategoryController.Response>> {
+    try {
+      const badParams = await this.validation.validate(request)
+      if (badParams) return new HttpError.BadRequest(badParams)
+
+      await this.removeCategory.remove(request)
+
+      return noContent()
+    } catch (error) {
+      return new HttpError.Server(String(error.stack))
+    }
+  }
+}
+
+export namespace RemoveCategoryController {
+  export type Request = RemoveCategory.Params
+  export type Response = RemoveCategory.Result
+}
