825c2cf0996704468d54f29ab85a051cff861868
DevLeuzera <le.tex@hotmail.com>
2022-03-03 22:42:36 -0300

    feat: add find category route

--- src/main/routes/category.ts
+++ src/main/routes/category.ts
@@ -1,8 +1,9 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeAddCategoryController } from '@/main/factories/controllers'
+import { makeAddCategoryController, makeFindCategoryController } from '@/main/factories/controllers'
 
 import { Router } from 'express'
 
 export const applyCategoryRoutes = (router: Router): void => {
+  router.get('/categories/:categoryId', adaptRoute(makeFindCategoryController()))
   router.post('/categories/add-category', adaptRoute(makeAddCategoryController()))
 }
