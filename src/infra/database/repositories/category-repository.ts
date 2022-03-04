5811900c29822252d353fd87ab8996b7d8617b49
DevLeuzera <le.tex@hotmail.com>
2022-03-03 22:31:08 -0300

    feat: add findCategory method

--- src/infra/database/repositories/category-repository.ts
+++ src/infra/database/repositories/category-repository.ts
@@ -1,9 +1,9 @@
 import { DatabaseError } from '@/data/errors'
-import { AddCategoryRepository } from '@/data/protocols'
+import { AddCategoryRepository, FindCategoryRepository } from '@/data/protocols'
 import { PgCategory } from '@/infra/database/entities'
 import { PostgresHelper } from '@/infra/database/helpers'
 
-export class PgCategoryRepository implements AddCategoryRepository {
+export class PgCategoryRepository implements AddCategoryRepository, FindCategoryRepository {
   async add (params: AddCategoryRepository.Params): Promise<AddCategoryRepository.Result> {
     try {
       const categoryRepository = await PostgresHelper.getRepository(PgCategory)
@@ -11,5 +11,17 @@ export class PgCategoryRepository implements AddCategoryRepository {
     } catch (error) {
       throw new DatabaseError.InsertFail(String(error.stack))
     }
+  }
+
+  async findCategory (params: FindCategoryRepository.Params): Promise<FindCategoryRepository.Result> {
+    const categoryRepository = await PostgresHelper.getRepository(PgCategory)
+
+    const category = await categoryRepository.findOne({
+      join: { alias: 'category', leftJoinAndSelect: { products: 'category.products' } },
+      where: { id: params.categoryId }
+    })
+
+    if (!category) throw new DatabaseError.NotFound(`"${params.categoryId}" could not be found`)
+    return category
   }
 }
