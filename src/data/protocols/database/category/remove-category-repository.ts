5aefd678d0c4d08801bced15b1286ba4a7021018
DevLeuzera <le.tex@hotmail.com>
2022-03-04 00:10:07 -0300

    feat: add RemoveCategoryRepository

---
+++ src/data/protocols/database/category/remove-category-repository.ts
@@ -0,0 +1,10 @@
+import { RemoveCategory } from '@/domain/usecases'
+
+export interface RemoveCategoryRepository {
+  removeCategory: (params: RemoveCategoryRepository.Params) => Promise<RemoveCategoryRepository.Result>
+}
+
+export namespace RemoveCategoryRepository {
+  export type Params = RemoveCategory.Params
+  export type Result = RemoveCategory.Result
+}
