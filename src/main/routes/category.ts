9a094795c338e575bf8ba6ca75638d6dda105c78
DevLeuzera <le.tex@hotmail.com>
2022-03-04 00:32:07 -0300

    feat: add remove category route

--- src/main/routes/category.ts
+++ src/main/routes/category.ts
@@ -1,5 +1,5 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeAddCategoryController, makeFindCategoryController, makeFindCategoriesController, makeEditCategoryController } from '@/main/factories/controllers'
+import { makeAddCategoryController, makeFindCategoryController, makeFindCategoriesController, makeEditCategoryController, makeRemoveCategoryController } from '@/main/factories/controllers'
 
 import { Router } from 'express'
 
@@ -8,4 +8,5 @@ export const applyCategoryRoutes = (router: Router): void => {
   router.get('/categories/:categoryId', adaptRoute(makeFindCategoryController()))
   router.post('/categories/add-category', adaptRoute(makeAddCategoryController()))
   router.put('/categories/edit-category/:categoryId', adaptRoute(makeEditCategoryController()))
+  router.delete('/categories/delete-category/:categoryId', adaptRoute(makeRemoveCategoryController()))
 }
