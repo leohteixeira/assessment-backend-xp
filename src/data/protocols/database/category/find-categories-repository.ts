768e4cb939da3a50ddbd6195e92e923703f2d27d
DevLeuzera <le.tex@hotmail.com>
2022-03-03 22:49:29 -0300

    feat: add FindCategoriesRepository

---
+++ src/data/protocols/database/category/find-categories-repository.ts
@@ -0,0 +1,10 @@
+import { FindCategories } from '@/domain/usecases'
+
+export interface FindCategoriesRepository {
+  findCategories: (params: FindCategoriesRepository.Params) => Promise<FindCategoriesRepository.Result>
+}
+
+export namespace FindCategoriesRepository {
+  export type Params = FindCategories.Params
+  export type Result = FindCategories.Result
+}
--- src/data/protocols/database/category/index.ts
+++ src/data/protocols/database/category/index.ts
@@ -1,2 +1,3 @@
 export * from './add-category-repository'
 export * from './find-category-repository'
+export * from './find-categories-repository'
