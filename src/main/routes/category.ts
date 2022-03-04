14f15093b6f1308342688f0f0117d6102b95b846
DevLeuzera <le.tex@hotmail.com>
2022-03-04 00:00:36 -0300

    feat: add edit category route

--- src/main/routes/category.ts
+++ src/main/routes/category.ts
@@ -1,5 +1,5 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeAddCategoryController, makeFindCategoryController, makeFindCategoriesController } from '@/main/factories/controllers'
+import { makeAddCategoryController, makeFindCategoryController, makeFindCategoriesController, makeEditCategoryController } from '@/main/factories/controllers'
 
 import { Router } from 'express'
 
@@ -7,4 +7,5 @@ export const applyCategoryRoutes = (router: Router): void => {
   router.get('/categories', adaptRoute(makeFindCategoriesController()))
   router.get('/categories/:categoryId', adaptRoute(makeFindCategoryController()))
   router.post('/categories/add-category', adaptRoute(makeAddCategoryController()))
+  router.put('/categories/edit-category/:categoryId', adaptRoute(makeEditCategoryController()))
 }
