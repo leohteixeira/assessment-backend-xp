c424caf1089175b1195775ebeb6b5f3cde3726e5
DevLeuzera <le.tex@hotmail.com>
2022-03-03 22:19:18 -0300

    feat: add DbFindCategory

---
+++ src/data/usecases/category/db-find-category.ts
@@ -0,0 +1,11 @@
+import { FindCategoryRepository } from '@/data/protocols'
+import { FindCategory } from '@/domain/usecases'
+
+export class DbFindCategory implements FindCategory {
+  constructor (private readonly findCategoryRepository: FindCategoryRepository) {}
+
+  async find (params: FindCategory.Params): Promise<FindCategory.Result> {
+    const category = await this.findCategoryRepository.findCategory(params)
+    return category
+  }
+}
