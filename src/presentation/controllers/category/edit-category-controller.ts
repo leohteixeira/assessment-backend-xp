9d2af1cbb7cb1740365a7d5aa6eda738bb0e4f4d
DevLeuzera <le.tex@hotmail.com>
2022-03-03 23:52:30 -0300

    feat: add EditCategoryController

---
+++ src/presentation/controllers/category/edit-category-controller.ts
@@ -0,0 +1,32 @@
+import { EditCategory } from '@/domain/usecases'
+import { HttpError } from '@/presentation/errors'
+import { ok } from '@/presentation/helpers'
+import { Http } from '@/presentation/protocols'
+import { Validation } from '@/validation/protocols'
+
+export class EditCategoryController implements Http.Controller {
+  constructor (
+    private readonly validation: Validation<EditCategoryController.Request>,
+    private readonly editCategory: EditCategory
+  ) {}
+
+  async handle (request: EditCategoryController.Request): Promise<Http.Response<EditCategoryController.Response>> {
+    try {
+      const badParams = await this.validation.validate(request)
+      if (badParams) return new HttpError.BadRequest(badParams)
+
+      const result = await this.editCategory.edit({
+        ...request
+      })
+
+      return ok(result)
+    } catch (error) {
+      return new HttpError.Server(String(error.stack))
+    }
+  }
+}
+
+export namespace EditCategoryController {
+  export type Request = EditCategory.Params
+  export type Response = EditCategory.Result
+}
--- src/presentation/controllers/category/index.ts
+++ src/presentation/controllers/category/index.ts
@@ -1,3 +1,4 @@
 export * from './add-category-controller'
 export * from './find-category-controller'
 export * from './find-categories-controller'
+export * from './edit-category-controller'
