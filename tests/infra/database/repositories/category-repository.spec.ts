fefad1dc74ee87224077a0860c993c14404764ea
DevLeuzera <le.tex@hotmail.com>
2022-03-03 22:31:30 -0300

    test: add findCategory method

--- tests/infra/database/repositories/category-repository.spec.ts
+++ tests/infra/database/repositories/category-repository.spec.ts
@@ -1,15 +1,16 @@
 import { DatabaseError } from '@/data/errors'
-import { PgCategory } from '@/infra/database/entities'
+import { PgCategory, PgProduct } from '@/infra/database/entities'
 import { PostgresHelper } from '@/infra/database/helpers'
 import { PgCategoryRepository } from '@/infra/database/repositories'
 import { testEnv } from '@/tests/utils'
 
 import { Repository } from 'typeorm'
-import { random } from 'faker'
+import { datatype, random } from 'faker'
 import MockDate from 'mockdate'
 import path from 'path'
 
 let categoryRepository: Repository<any>
+let productRepository: Repository<any>
 
 const makeSut = (): PgCategoryRepository => {
   return new PgCategoryRepository()
@@ -29,6 +30,8 @@ describe('PgCategoryRepository', () => {
   beforeEach(async () => {
     categoryRepository = await PostgresHelper.getRepository(PgCategory)
     await categoryRepository.delete({})
+    productRepository = await PostgresHelper.getRepository(PgProduct)
+    await productRepository.delete({})
   })
 
   describe('add()', () => {
@@ -54,6 +57,35 @@ describe('PgCategoryRepository', () => {
       })
       expect(category).toBeTruthy()
       expect(category.id).toBeTruthy()
+    })
+  })
+
+  describe('findCategory()', () => {
+    test('Should throw DatabaseError.NotFound if the category could not be found', async () => {
+      const categoryId = datatype.uuid()
+      const sut = makeSut()
+      const promise = sut.findCategory({ categoryId })
+      await expect(promise).rejects.toThrowError(DatabaseError.NotFound)
+    })
+
+    test('Should return Category.Model on success', async () => {
+      const category = await categoryRepository.save({
+        name: random.words(),
+        code: random.words()
+      })
+      await productRepository.save({
+        name: random.words(),
+        sku: random.words(),
+        price: datatype.number({ min: 1 }),
+        description: random.words(),
+        quantity: datatype.number({ min: 1 }),
+        categories: [category]
+      })
+      const sut = makeSut()
+      const foundCategory = await sut.findCategory({ categoryId: category.id })
+      expect(foundCategory).toBeTruthy()
+      expect(foundCategory.id).toBeTruthy()
+      expect(foundCategory.id).toBe(category.id)
     })
   })
 })
